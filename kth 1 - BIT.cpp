a570b4496913fa6846e656d729af1e2de6d76d3f
Munawar Hussain Shakil <shakil.ruet.12@gmail.com>
2016-11-20 19:16:01 +0600

    Create kth 1 - BIT.cpp

---
+++ kth 1 - BIT.cpp
@@ -0,0 +1,33 @@
+#define vi vector<int>
+
+#define LSOne(S) (S & (-S))
+class FenwickTree {
+private:
+  vi ft;
+int d;
+public:
+  FenwickTree() {}
+  // initialization: n + 1 zeroes, ignore index 0
+  FenwickTree(int n) {d=n; ft.assign(n + 1, 0); }
+
+  int kth(int v){
+      int m=d,a=d,ans=-1;
+        while(a)
+        {
+            a>>=1;
+            if(ft[m]>=v)
+            {
+                if(ft[m]==v) ans=m;
+                m-=a;
+            }
+            else {
+                v-=ft[m];
+            m+=a;
+            }
+        }
+        return ans;
+  }
+  // adjusts value of the k-th element by v (v can be +ve/inc or -ve/dec)
+  void adjust(int k, int v) {                    // note: n = ft.size() - 1
+    for (; k < (int)ft.size(); k += LSOne(k)) ft[k] += v; }
+};
